558ce64cf70fca4e07b716db2208d9805581608a
Matthew Pfeiffer <spferical@gmail.com>
2016-03-11 22:06:20 -0500

    Tweak action delay & ap regen time

--- core/src/main/animated/spferical/netrogue/Constants.java
+++ core/src/main/animated/spferical/netrogue/Constants.java
@@ -1,7 +1,7 @@
 package animated.spferical.netrogue;
 
 public class Constants {
-	public static final float actionDelay = 0.1f; // in seconds
+	public static final float actionDelay = 0.25f; // in seconds
 	public static final int tileSize = 64;
 	public static final int chunkSize = 16;
 	
@@ -11,7 +11,7 @@ public class Constants {
 	public static final int BASE_AP = 5;
 	public static final int EXTRA_AP_PER_LEVEL = 1;
 	
-	public static final float AP_REGEN_TIME = 0.25f;
+	public static final float AP_REGEN_TIME = 1f/3;
 	public static final float AP_REGEN_AMOUNT = 1;
 	
 	// New Level = log2(XP / 20) + 1
